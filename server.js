4046d5238b1714f8ed48087981aa91c62a6084b4
Whichhunter404 <schmidt.peti32@gmail.com>
2020-02-29 14:04:18 +0100

    can't add existed user

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.post('/signin',(req,res)=>{
 app.post('/register',(req,res)=>{
     const {name,email,password} = req.body;
     const hash = bcrypt.hashSync(password,10);
-    const user_withthis_email = db('users').where('name', name).andWhere('email', email).select('id');
+    const user_withthis_email = db('users').where('name', name).andWhere('email', email).select('id').then(user => user);
     const isUserExists = (0 < user_withthis_email.length);
     if(!isUserExists){
         db('users').returning('*').insert({
